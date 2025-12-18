4fe8d5bbede12a8dd047196d1ef9ef1b5e1fcce1
Nischal Puri <mrnischalpuri@gmail.com>
2025-12-18 11:41:44 +0545

    added comment

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import MainContent from "./components/MainContent"
+import logo from "./assets/images/logo.png"
+
+function App() {
+  return (
+    <>
+      {/* Just a comment */}
+      <header>
+        <div id="logo-img">
+          <img src={logo} />
+        </div>
+        <h1>Learn & Master GitHub Actions</h1>
+      </header>
+      <MainContent />
+    </>
+  )
+}
+
+export default App
